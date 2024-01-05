d3249f38e471a1c71897824321d02ff9f7dfeb56
Mr. Hasan <hasanshahriar32@gmail.com>
2024-01-05 22:00:20 +0600

    Update import statements in Hero component

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,8 +1,8 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import PwaInstall from '@/components/Shared/PwaInstall';
-import VideoBanner from '@/components/Shared/VideoBanner';
-import { Button } from '@/components/ui/button';
+import PwaInstall from "@/components/Shared/PwaInstall";
+import VideoBanner from "@/components/Shared/VideoBanner";
+import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
@@ -12,10 +12,14 @@ export default function Hero() {
           <div className="flex flex-col justify-center space--4 text-center">
             <div className="space-y-2 mb-6">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
-                Nextra With Extra (NWX)
+                Nextra With{" "}
+                <span className="text-destructive-foreground">Extra</span> (NWX)
               </h1>
               <p className="max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto">
-                Take Documentation System on the Next Level⚡.
+                Take Documentation System to the Next Level⚡. Have an project
+                that might need a documentation system? Nextra With Extra (NWX)
+                is the perfect choice for you. We have added some extra features
+                to the Nextra theme to make it more powerful and easy to use.
               </p>
             </div>
             <div className="w-full max-w-sm space-y-2 mx-auto">
