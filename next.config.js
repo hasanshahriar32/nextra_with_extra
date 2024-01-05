1d19bebecb11af43e1fca7e88dc98edd3edb2724
Mr. Hasan <hasanshahriar32@gmail.com>
2024-01-05 17:13:54 +0600

    Add next-pwa and webpack dependencies, configure next.config.js for PWA

--- next.config.js
+++ next.config.js
@@ -3,8 +3,19 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
-
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  // disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 const nextConfig = {
   reactStrictMode: true,
 };
-module.exports = withNextra({ nextConfig });
+module.exports = withPWA(withNextra({ nextConfig }));
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import Hero from "@/components/Home/Hero";
 import { Features } from "@/components/component/features";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
