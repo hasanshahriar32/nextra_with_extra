29bd066f0dd78ba2ec7721a51efa6ebaabff0b21
Mr. Hasan <hasanshahriar32@gmail.com>
2024-01-08 22:28:47 +0600

    Refactor Hero component layout

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,12 +1,13 @@
-
 import VideoBanner from "@/components/Shared/VideoBanner";
 
 export default function Hero() {
   return (
-    <section className="w-full flex justify-center">
-      <div className="min-w-[90vw] md:min-w-[80vw] mx-5 md:mx-10 max-w-4xl">
-        <VideoBanner />
-      </div>
-    </section>
+    <div className="container flex h-full min-h-[calc(100vh-140px)] w-full flex-col items-center justify-center space-y-20 py-14 md:min-h-[calc(100vh-160px)] xl:pt-24">
+      <section className="w-full flex justify-center">
+        <div className="min-w-[90vw] md:min-w-[80vw] mx-5 md:mx-10 max-w-4xl">
+          <VideoBanner />
+        </div>
+      </section>
+    </div>
   );
 }
