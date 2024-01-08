5b8f3a1143c13665bf949ecb59598cc8e1a4aeae
Mr. Hasan <hasanshahriar32@gmail.com>
2024-01-08 21:09:26 +0600

    Add dependencies and update logo
    Update authentication provider and adapter

--- src/components/Shared/Nav/Navbar.jsx
+++ src/components/Shared/Nav/Navbar.jsx
@@ -15,12 +15,12 @@ export default function Navbar({}) {
       <Link href="/" className="flex items-center text-2xl font-bold">
         <Image
           src="/favicon.ico"
-          alt="NPX logo"
+          alt="NWX logo"
           width="30"
           height="30"
           className="mr-2 rounded-sm object-contain"
         />
-        <p>NPX</p>
+        <p>NWX</p>
       </Link>
       <div className="hidden items-center gap-12 md:flex 2xl:gap-16">
         <div className="space-x-4 text-center text-sm leading-loose text-muted-foreground md:text-left">
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,18 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  }),
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
   session: {
